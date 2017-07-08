2c3a120b465029caf1825e239f9b16dc29e6030d
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-07-08 13:23:01 -0700

    Initial setup of database+ SQLAlchemy

--- get-it-done/main.py
+++ get-it-done/main.py
@@ -1,9 +1,25 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+
+db = SQLAlchemy(app)
 
 tasks = []
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -15,4 +31,5 @@ def index():
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
