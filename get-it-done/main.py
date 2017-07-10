cc3a4011efeb48f55c19ce250a6969009e6ee22d
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-07-09 20:34:38 -0700

    corrected completed tasks bug, app working correctly pt5 complete

--- get-it-done/main.py
+++ get-it-done/main.py
@@ -21,6 +21,18 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
