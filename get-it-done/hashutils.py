6e674f52e65be303081ea0180fb517efad9ba0df
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-07-21 18:04:26 -0700

    Salted the hash

--- get-it-done/hashutils.py
+++ get-it-done/hashutils.py
@@ -1,13 +1,25 @@
 import hashlib
+import random
+import string
 
-# Hash user's password 
-def make_pw_hash(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
+# Makes a random string of letters 5 letters in length
+def make_salt():
+    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
 
 
-# verify entered password against previously hashed password in db
+# Hash user's password, salt it and store both the pw and the salt
+
+def make_pw_hash(password, salt=None):
+    if not salt:
+        salt = make_salt()
+    hash = hashlib.sha256(str.encode(password)).hexdigest()
+    return '{0},{1}'.format(hash, salt)
+
+
+# verify entered password against previously hashed/salted password in db
 def check_pw_hash(password, hash):
-    if make_pw_hash(password) == hash:
+    salt = hash.split(',')[1]
+    if make_pw_hash(password, salt) == hash:
         return True
 
     return False
